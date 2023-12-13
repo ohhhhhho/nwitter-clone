e5a5b364887edbe23484d3df5fab21fe0139bf0d
오연주 <yeonjuoh@YEONJUui-Mac-Studio.local>
2023-12-13 17:27:45 +0900

    게시글 form 만들기

--- src/components/post-tweet-fotm.tsx
+++ src/components/post-tweet-fotm.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Form = styled.form`
@@ -22,19 +23,65 @@ const TextArea = styled.textarea`
     border-color: #1d9bf0;
   }
 `;
-const AttatchFileButton = styled.label``;
+const AttatchFileButton = styled.label`
+  padding: 10px 0px;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  border: solid 1px #1d9bf0;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+`;
 const AttatchFileInput = styled.input`
   display: none;
 `;
-const SubmitBtn = styled.input``;
+const SubmitBtn = styled.input`
+  background-color: #1d9bf0;
+  border: none;
+  color: white;
+  padding: 10px 0;
+  border-radius: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.8;
+  }
+`;
 
 export default function PostTweetForm() {
+  const [isLoading, setLoding] = useState(false);
+  const [tweet, setTweet] = useState("");
+  const [file, setFile] = useState<File | null>(null); // TypeScript구문
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTweet(e.target.value);
+  };
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (files && files.length === 1) {
+      setFile(files[0]);
+    }
+  };
   return (
     <Form>
-      <TextArea placeholder="What is happening?" />
-      <AttatchFileButton htmlFor="file">Add Photo</AttatchFileButton>
-      <AttatchFileInput type="file" id="file" accept="image/*" />
-      <SubmitBtn type="submit" value="Post Tweet" />
+      <TextArea
+        rows={5}
+        maxLength={180}
+        onChange={onChange}
+        value={tweet}
+        placeholder="What is happening?"
+      />
+      <AttatchFileButton htmlFor="file">
+        {file ? "Photo added" : "Add Photo"}
+      </AttatchFileButton>
+      <AttatchFileInput
+        onChange={onFileChange}
+        type="file"
+        id="file"
+        accept="image/*"
+      />
+      <SubmitBtn type="submit" value={isLoading ? "Posting." : "Post Tweet"} />
     </Form>
   );
 }
